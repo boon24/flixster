9ad771bf40213650bb9c142c1cdbcd03c3e4d0e0
Haroon Zia <haroonzia24@gmail.com>
2020-01-21 19:19:56 -0800

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Kay Lab on 1/21/20.
+//  Copyright © 2020 Haroon Zia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
