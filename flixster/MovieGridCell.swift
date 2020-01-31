0cb87b7f02fdf50e5a38882401dbe2ef3126aac4
Haroon Zia <haroonzia24@gmail.com>
2020-01-30 23:41:52 -0800

    added part 2 features

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Kay Lab on 1/30/20.
+//  Copyright © 2020 Haroon Zia. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
